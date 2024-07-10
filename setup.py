26c535402c51272957cfa85a0ea01aa5a368ddf3
Jeff Trawick <trawick@gmail.com>
2024-07-10 16:58:54 -0400

    fix some setup.py metadata

--- setup.py
+++ setup.py
@@ -45,10 +45,13 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     install_requires=[
         'requests', 'PyYAML',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
     scripts=('out_of_date.py',),
 )
